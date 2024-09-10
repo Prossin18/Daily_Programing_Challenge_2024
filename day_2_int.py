be8e603be35e82669ade2083075e34ac20180f25
Prossin18 <sangaleatul143@gmail.com>
2024-09-10 23:44:55 +0530

    Update day_2_int.py

--- day_2_int.py
+++ day_2_int.py
@@ -1,16 +1,4 @@
 
-
-
-
-#arr = [1,2,3,4]
-
-#for i in range(0,len(arr)+1):
-  #  if i+1 != arr[i]:
-        
-        
-    
-    #    print(i+1)
-      #  break
 arr = [1, 2, 3, 4]
 
 # Iterate from 1 to len(arr) + 1
